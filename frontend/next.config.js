ebd34ddc434a1940db1372d5535740fa063670f8
Bakemono <82776147+Bakemono-san@users.noreply.github.com>
2026-07-24 14:41:13 +0200

    Remove output:standalone, simplify rewrite

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,16 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
 
-  output: "standalone",
-
-  // Configuration API
   async rewrites() {
     return [
       {
         source: "/api/:path*",
-        destination: process.env.NEXT_PUBLIC_API_URL
-          ? `${process.env.NEXT_PUBLIC_API_URL}/api/:path*`
-          : "https://vendor-person-television-increases.trycloudflare.com/api/:path*",
+        destination: "https://vendor-person-television-increases.trycloudflare.com/api/:path*",
       },
     ];
   },
